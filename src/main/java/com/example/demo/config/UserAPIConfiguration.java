8362a723e2ce31d94427041a1ccb440bcf4c0d43
Brian Tang <brian.th.tang.pccw@gmail.com>
2020-10-20 11:18:33 +0800

    Answers removed

--- src/main/java/com/example/demo/config/UserAPIConfiguration.java
+++ src/main/java/com/example/demo/config/UserAPIConfiguration.java
@@ -1,17 +1,10 @@
 package com.example.demo.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserAPIConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setAmbiguityIgnored(true);
-        return modelMapper;
-    }
+    //Define any necessary bean here
 
 }
--- src/main/java/com/example/demo/restservice/UserController.java
+++ src/main/java/com/example/demo/restservice/UserController.java
@@ -1,38 +1,11 @@
 package com.example.demo.restservice;
 
-import com.example.demo.dto.UserDto;
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.vo.CreateUserRequestVo;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
-
-    @PostMapping("/create")
-    public UserDto greeting(@RequestBody CreateUserRequestVo createUserRequestVo) {
-
-        User targetUser = modelMapper.map(createUserRequestVo, User.class);
-
-        List<User> results = userRepository.findById1AndId2(targetUser.getId1(), targetUser.getId2());
-
-        if(results.size() > 0){
-            return modelMapper.map(results.get(0), UserDto.class);
-        }else{
-            userRepository.save(targetUser);
-            return modelMapper.map(targetUser, UserDto.class);
-        }
-    }
+    //Define the post method here
 
 }
